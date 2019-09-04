3f8816f95b3371f2e3950ba3661f7f7ebec98335
dark-kuro <33413554+dark-kuro@users.noreply.github.com>
2019-09-04 12:20:31 +0545

    Update sankaku-thumbs-notify.user.js

--- sankaku-thumbs-notify.user.js
+++ sankaku-thumbs-notify.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         sankaku-thumbs-notify.user.js
-// @version      1.0
+// @version      1.1
 // @description  Notify The Current Number of Thumbs Loaded
 // @author       Dark.Kuro
 // @match        https://*.sankakucomplex.com/?tags=*
@@ -12,8 +12,8 @@ notifier.id = '#notify-thumbs-count';
 notifier.style.color = '#FFF';
 notifier.style.backgroundColor = 'rgba(0,0,0,0.5)';
 notifier.style.fontSize = '50px';
-notifier.style.bottom = '0px';
-notifier.style.right ='0px';
+notifier.style.bottom = '50px';
+notifier.style.right ='10px';
 notifier.style.position= 'fixed';
 notifier.style.height = '50px';
 
